f0902ac662bbc8e7784279546b78ae8ecc4821e7
filipegiusti <filipegiusti@ed8988b3-1941-0410-a981-6d549a63f054>
2008-01-16 18:40:55 +0000

    Ajustado Makefile e Adicionado TODO's no hreset.c

--- sistema_arquivo/src/hreset/hreset.c
+++ sistema_arquivo/src/hreset/hreset.c
@@ -14,29 +14,41 @@
 int main(int agrc, char *argv[]) {
 	pid_t pid;
 	pid = fork();
+	// TODO: Validar os parametros passados pelo usuário
+	// TODO: Criar help caso parametros sejam passados errados
+	// TODO: Verificar se boot está válido
+	// TODO: Criar arquivo com os primeiros 1024 bytes do boot
+
+	/* Inicio da Execução do código do filho */
 	if (pid == 0) {
-		char *parametros[] = {"boot.bin", "disco2" , (char *)0 };
-		printf("Sou o filho\n");
+		char *parametros[] = {"boot.bin", argv[1] , (char *)0 };
+		// TODO: trocar funçaõ para execvp que recebe um descritor de arquivo
 		if (execv("boot.bin", parametros) == -1) {
+			// TODO: Verificar a causa do erro do execv
 			printf("Erro no execv.\n");
 		}
 	}
+	/* Erro no fork() */
 	else if (pid == -1) {
+		// TODO: Melhorar o erro
 		printf("Deu erro.\n");
 	}
+	/* Início da execução do código do pai */
 	else {
 		int filho_status;
 		if ( waitpid(pid, &filho_status, 0) == -1 ) {
+			// TODO: Verificar causa do erro no waitpid
 			printf("Deu erro no waitpid.\n");
 		}
+		/* Filho finalizou OK */
 		if (WIFEXITED(filho_status)) {
-			printf("Filho finalizou LEGAL\n");
+			execv("temp.exe", NULL);
 			
 		}
 		else {
+			// TODO: Melhorar o erro
 			printf("Problema na finalização do filho.\n");
 		}
-		printf("Sou o pai\n");
 	}
 	return 0;
 }
