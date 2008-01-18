f52be8763a65e894e0ab77aa70c0c35cdf4aeff7
filipegiusti <filipegiusti@ed8988b3-1941-0410-a981-6d549a63f054>
2008-01-18 03:56:47 +0000

    Movido cabeçalho do hformat.h pra dentro do hformat.c Criado uma lista de TODO's

--- trunk/sistema_arquivo/src/hformat/hformat.c
+++ trunk/sistema_arquivo/src/hformat/hformat.c
@@ -6,7 +6,29 @@
  * @date    16/01/2007
  */
 
-#include "../../include/hformat.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+#include "util.h"
+
+#define BOOT1 0x33 /**< Define o conteúdo do penúltimo setor de boot. */
+#define BOOT2 0xCC /**< Define o conteúdo do último setor de boot. */
+#define BOOT_PATH "boot.bin" /**< Define a localização do arquivo que contém as informações do boot. */
+#define HPSYS_PATH "hpsys" /**< Define a localização do HPSYS. */
+#define SIZE_SEC 512 /**< Define o tamanho de segmento a ser utilizado no sistema de arquivos */
+#define MIN_DISK_SIZE ( 512 * SIZE_SEC ) /**< Define o tamanho mínimo em bytes que um disco a ser formatado deve ter */
+#define MAX_DISK_SIZE ( 4096 * SIZE_SEC ) /**< Define o tamanho máximo em bytes que um disco a ser formatado poderá ter */
+
+struct stat info_disk; /**< Estrutura que armazena as informações de estado do arquivo que representa o disco */
+char DISK_NAME[255]; /**< Variável que irá conter o nome do disco a ser utilizado */
+FILE *disk; /**< Variável utilizada para referenciar o disco que estará sendo utilizado */
+
+int save_boot(short int tam_hpsys);
+short int save_so();
+int save_bitmap(short int tam_hpsys);
+int save_root(short int tam_hpsys);
+int format_disk();
 
 /**
  * @fn int save_boot(int tam_hpsys)
@@ -42,11 +64,9 @@ int save_boot(short int tam_hpsys) {
 	fclose(boot_file);
 	
 	/* Grava o código do boot e o tamanho do hpsys */
-	fseek(disk, (2*SIZE_SEC)-6-1, SEEK_SET); /* 6 é a soma de 2 char + 2 short int e -1 pra acertar a referencia */
-	fwrite(2+tam_hpsys, sizeof(short int), 1, disk); //TODO: Tratar erro
+	fseek(disk, (2*SIZE_SEC)-6-1, SEEK_SET); /* 6 é a soma de 2 short int mais 2 bytes pros marcadores e -1 pra acertar a referência */
+	fwrite(tam_hpsys+2, sizeof(short int), 1, disk); /* Início bitmap de setores livres *///TODO: Tratar erro
 	fwrite(tam_hpsys, sizeof(short int), 1, disk); //TODO: Tratar erro
-	fwrite(BOOT1, sizeof(char), 1, disk); //TODO: Tratar erro
-	fwrite(BOOT2, sizeof(char), 1, disk); //TODO: Tratar erro
 
 	debug("BOOT salvo");
 	return 1;
