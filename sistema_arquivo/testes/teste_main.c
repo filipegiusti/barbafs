8d1a2c07ce2b862132a271a5842086fea58dc970
filipegiusti <filipegiusti@ed8988b3-1941-0410-a981-6d549a63f054>
2008-08-13 03:34:56 +0000

    Sai fora fatorial!!!!!!! Perfumarias.

--- sistema_arquivo/testes/teste_main.c
+++ sistema_arquivo/testes/teste_main.c
@@ -8,13 +8,13 @@ Suite *hformat_suite();
 
 int main (int argc, char *argv[]) {
 	int number_failed;
-	Suite *fatorial = fatorial_suite();
+	//Suite *fatorial = fatorial_suite();
 	Suite *filesystem = filesystem_suite();
-	Suite *hformat = hformat_suite();
+	//Suite *hformat = hformat_suite();
 	//Suite *filesystem = filesystem_suite();
 	SRunner *sr = srunner_create(filesystem);
-	srunner_add_suite(sr, hformat);
-	srunner_add_suite(sr, fatorial);
+	//srunner_add_suite(sr, hformat);
+	//srunner_add_suite(sr, fatorial);
 	srunner_run_all (sr, CK_NORMAL);
 	number_failed = srunner_ntests_failed (sr);
 	srunner_free (sr);
